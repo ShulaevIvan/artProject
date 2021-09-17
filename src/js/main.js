8ef51f9838493157ab4060c24bae1e15b141efe0
Shulaev Ivan <mail@ntdservice.ru>
2021-09-17 13:32:42 +0300

    add func

---
+++ src/js/main.js
@@ -0,0 +1,20 @@
+import modals from './modules/modals';
+import sliders from './modules/sliders';
+import forms from './modules/forms';
+import mask from './modules/mask';
+import showMoreStyles from './modules/showMoreStyles';
+import calc from './modules/calc';
+import filter from './modules/filter';
+
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+
+    modals();
+    sliders('.feedback-slider-item', 'horizontal', '.main-prev-btn', '.main-next-btn');
+    sliders('.main-slider-item', 'vertical');
+    forms();
+    mask('[name="phone"]');
+    showMoreStyles('.button-styles', '.styles-2');
+    calc('#size', '#material', '#options', '.promocode', '.calc-price');
+    filter();
+});
