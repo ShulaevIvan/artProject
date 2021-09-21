da08b01e85093be7eeb1ea246c4e26dd93ec3c6b
Shulaev Ivan <mail@ntdservice.ru>
2021-09-21 16:21:00 +0300

    addFunc2

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,8 @@ import mask from './modules/mask';
 import showMoreStyles from './modules/showMoreStyles';
 import calc from './modules/calc';
 import filter from './modules/filter';
+import pictureSize from './modules/pictureSize';
+import accordion from './modules/accordion';
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -17,4 +19,6 @@ window.addEventListener('DOMContentLoaded', () => {
     showMoreStyles('.button-styles', '.styles-2');
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
+    pictureSize('.sizes-block');
+    accordion('.accordion-heading');
 });
